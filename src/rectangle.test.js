c2f634fc9a5c52965ae77a3f2eb9ed11beddb896
Agata Grzelczak <agata.grzelczak@deviniti.com>
2023-10-24 12:28:17 +0200

    modified description

--- src/rectangle.test.js
+++ src/rectangle.test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { Rectangle } from './Rectangle';
 
-test('should create new object with proper field values', () => {
+test('Should create new object with proper field values', () => {
     
 })
